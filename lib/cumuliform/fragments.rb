8465059ac41dce8475436e4181ef53c6f254a731
Matt Patterson <matt@reprocessed.org>
2014-12-01 19:05:07 +0100

    Add fragment import support
    Now fragments are imported (and overrideable) too

--- lib/cumuliform/fragments.rb
+++ lib/cumuliform/fragments.rb
@@ -14,6 +14,17 @@ module Cumuliform
       end
     end
 
+    def find_fragment(name)
+      local_fragment = fragments[name]
+      imports.reverse.reduce(local_fragment) { |fragment, import|
+        fragment || import.find_fragment(name)
+      }
+    end
+
+    def has_fragment?(name)
+      !find_fragment(name).nil?
+    end
+
     private
 
     def define_fragment(name, block)
@@ -24,10 +35,10 @@ module Cumuliform
     end
 
     def include_fragment(name, *args)
-      unless fragments.has_key?(name)
+      unless has_fragment?(name)
         raise Error::FragmentNotFound, name
       end
-      instance_exec(*args, &fragments[name])
+      instance_exec(*args, &find_fragment(name))
     end
   end
 end
--- spec/cumuliform/import_spec.rb
+++ spec/cumuliform/import_spec.rb
@@ -26,6 +26,24 @@ describe "Template importing" do
 
       expect(importing_template.to_hash).to eq({"Resources" => {"Eg" => "Other"}})
     end
+
+    it "does NOT preserve the defining context when referring to fragments" do
+      base_template = Cumuliform.template {
+        fragment(:ref) { "Hello" }
+
+        resource("Eg") do
+          fragment(:ref)
+        end
+      }
+
+      importing_template = Cumuliform.template {
+        import base_template
+
+        fragment(:ref) { "Value" }
+      }
+
+      expect(importing_template.to_hash).to eq({"Resources" => {"Eg" => "Value"}})
+    end
   end
 
   context "verifying lookups" do
@@ -51,6 +69,65 @@ describe "Template importing" do
       }
 
       expect(importing_template.xref("Eg")).to eq("Eg")
+    end
+  end
+
+  context "fragments" do
+    it "gives access to an imported fragment" do
+      base_template = Cumuliform.template {
+        fragment(:eg) {
+          "Value"
+        }
+      }
+
+      importing_template = Cumuliform.template {
+        import base_template
+
+        resource("Eg") do
+          fragment(:eg)
+        end
+      }
+
+      expect(importing_template.to_hash).to eq({"Resources" => {"Eg" => "Value"}})
+    end
+
+    it "finds imported fragments when other fragments are referred" do
+      base_template = Cumuliform.template {
+        fragment(:ref) { "hey" }
+        fragment(:eg) {
+          {k: fragment(:ref)}
+        }
+      }
+
+      importing_template = Cumuliform.template {
+        import base_template
+
+        resource("Eg") do
+          fragment(:eg)
+        end
+      }
+
+      expect(importing_template.to_hash).to eq({"Resources" => {"Eg" => {k: "hey"}}})
+    end
+
+    it "does NOT preserve the defining context when fragments ref other fragments" do
+      base_template = Cumuliform.template {
+        fragment(:ref) { "hey" }
+        fragment(:eg) {
+          {k: fragment(:ref)}
+        }
+      }
+
+      importing_template = Cumuliform.template {
+        import base_template
+
+        fragment(:ref) { "ho" }
+        resource("Eg") do
+          fragment(:eg)
+        end
+      }
+
+      expect(importing_template.to_hash).to eq({"Resources" => {"Eg" => {k: "ho"}}})
     end
   end
 end
